90d90be273550e1a4ab7d5fa12892d810df25678
Mathys PIERREZ <mathyspierrez2@gmail.com>
2022-01-27 18:08:04 +0100

    Optimisation du code et ajout de fonctions

--- programme.java
+++ programme.java
@@ -1,25 +1,36 @@
 import java.util.Scanner;
 
 import Ex1.Ex1;
-import Ex2.Ex2;
-import Year.Year;
+import java.time.Year;
 
 public class programme
 {
     public static void main(String[] args)
     {
+        Ex1 cas = new Ex1();
+
         Scanner sc = new Scanner(System.in);
         System.out.print("Veuillez saisir une année: ");
         Year year = Year.parse(sc.nextLine());
+
+        showLeap(year);
+        showNumberDays(year);
+        
+        cas.showPreviousLeap(year);
+        cas.showNextLeap(year);
+    }
+
+    public static void showLeap(Year year)
+    {
         if (year.isLeap()) {
             System.out.println("Cette année est bissextile");
         } else {
             System.out.println("Cette année n'est pas bissextile");
         }
+    }
+
+    public static void showNumberDays(Year year)
+    {
         System.out.println("Cette année contient " + year.length() + " jours");
-        Ex1 cas1 = new Ex1();
-        Ex2 cas2 = new Ex2();
-        cas1.showPreviousLeap(year);
-        cas2.showNextLeap(year);
     }
 }
